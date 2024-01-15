699d74ee3b1ed81a699e2434a04ef607ca03bb4c
mababio <michaelkwasi@gmail.com>
2024-01-14 21:28:00 -0500

    Clean up code

--- main.py
+++ main.py
@@ -1,34 +1,72 @@
-from concurrent.futures import TimeoutError
-from google.cloud import pubsub_v1
-from urllib import request, parse
-from relay import garage
-import util.notification as notification
-import base64
+import random
+from paho.mqtt import client as mqtt_client
+from util.config import settings
+import RPi.GPIO as GPIO
+import time, sys
+from util import validator
+
+broker = settings['mqtt']['broker']
+port = 8883
+topic = "garage/command"
+client_id = f'garage-command-{random.randint(0, 100)}'
+username = settings['mqtt']['username']
+password = settings['mqtt']['password']
 
 
-timeout = 5.0
+# TODO: Maybe set up logging sink
+def connect_mqtt() -> mqtt_client:
+    """
+    Connecting to mqtt  garage topic
+    :return: mqtt client object
+    """
 
-subscriber = pubsub_v1.SubscriberClient()
-project_id = "ensure-dev-zone"
-subscription_id = "garage-control-sub"
-subscription_path = subscriber.subscription_path(project_id, subscription_id)
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
 
-def callback(message: pubsub_v1.subscriber.message.Message) -> None:
-    print(f"Received {message}.")
-    message.ack()
-    pubsub_message = message.data.decode()
-    garage(pubsub_message)
+    client = mqtt_client.Client(client_id)
+    client.tls_set(ca_certs='./server-ca.crt')
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
 
-streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-print(f"Listening for messages on {subscription_path}..\n")
 
-# Wrap subscriber in a 'with' block to automatically call close() when done.
-with subscriber:
-    try:
-        # When `timeout` is not set, result() will block indefinitely,
-        # unless an exception is encountered first.
-        streaming_pull_future.result()#timeout=timeout)
-    except TimeoutError:
-        streaming_pull_future.cancel()  # Trigger the shutdown.
-        streaming_pull_future.result()  # Block until the shutdown is complete.
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        if msg.payload.decode() == "open" and not validator.garage_is_open():
+            garage_relay()
+        elif msg.payload.decode() == "closed" and validator.garage_is_open():
+            garage_relay()
 
+    client.subscribe(topic)
+    client.on_message = on_message
+
+    # notification.send_push_notification("Raspberry Pi  ::::: Opening Garage door")
+
+
+def garage_relay():
+    """
+    Low level controls to open/close garage
+    """
+    your_board_gpio = 4
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(your_board_gpio, GPIO.OUT)
+    GPIO.setwarnings(False)
+    GPIO.output(your_board_gpio, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(your_board_gpio, GPIO.LOW)
+    GPIO.cleanup(your_board_gpio)
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
---
+++ notification.py
@@ -0,0 +1,13 @@
+from urllib import request, parse
+from util.config import settings
+
+def send_push_notification(message):
+    ['production']['key']['chanify']
+    message_json = {'text': message}
+    data = parse.urlencode(message_json).encode()
+    req = request.Request("https://api.chanify.net/v1/sender/" + token, data=data)
+    request.urlopen(req)
+
+
+if __name__ == "__main__":
+    send_push_notification( 'testing')
