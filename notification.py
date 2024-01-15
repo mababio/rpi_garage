6249eb0ed68ed1c585a86f639d1ad0e11d1a0b52
mababio <michaelkwasi@gmail.com>
2024-01-15 00:36:16 -0500

    gearing out for first run

--- notification.py
+++ notification.py
@@ -1,8 +1,11 @@
+from os import environ
 from urllib import request, parse
+
+TOKEN = environ.get('CHANIFY_KEY')
 
 
 def send_push_notification(message):
-    token = "examplePass"
+    
     message_json = {'text': message}
     data = parse.urlencode(message_json).encode()
     req = request.Request("https://api.chanify.net/v1/sender/" + token, data=data)
