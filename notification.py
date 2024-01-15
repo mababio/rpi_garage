c9d64ad56ab1372c10017ad7707bf017ecc360eb
mababio <michaelkwasi@gmail.com>
2024-01-15 00:11:41 -0500

    gearing out for first run

--- notification.py
+++ notification.py
@@ -1,9 +1,8 @@
 from urllib import request, parse
-from util.config import settings
 
 
 def send_push_notification(message):
-    token = "***REMOVED***"
+    token = "CIDEx7AGEiJBRERKNklQMlJNSEpaSkhSSEdOR1pIUEE0NUlQQUFNNENVIgIIAQ.kjUkfcqsKdw7KRH1RcS1u_PqtZyNqvXPNS-dj6b43Fg"
     message_json = {'text': message}
     data = parse.urlencode(message_json).encode()
     req = request.Request("https://api.chanify.net/v1/sender/" + token, data=data)
