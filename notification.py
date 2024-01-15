2e9329821df2e48a243897525909a465524c70ae
mababio <michaelkwasi@gmail.com>
2024-01-15 00:11:41 -0500

    gearing out for first run

--- notification.py
+++ notification.py
@@ -1,8 +1,8 @@
 from urllib import request, parse
-from util.config import settings
+
 
 def send_push_notification(message):
-    ['production']['key']['chanify']
+    token = "examplePass"
     message_json = {'text': message}
     data = parse.urlencode(message_json).encode()
     req = request.Request("https://api.chanify.net/v1/sender/" + token, data=data)
@@ -10,4 +10,4 @@ def send_push_notification(message):
 
 
 if __name__ == "__main__":
-    send_push_notification( 'testing')
+    send_push_notification('testing')
