992cac7c2495a1788bd981c15017b8377a54334f
mababio <michaelkwasi@gmail.com>
2024-01-16 16:49:34 -0500

    Clean up code

--- notification.py
+++ notification.py
@@ -1,14 +1,19 @@
+import os
 from os import environ
 from urllib import request, parse
 
-TOKEN = environ.get('CHANIFY_KEY')
+try:
+    TOKEN = os.environ['CHANIFY_KEY']
+except KeyError:
+    print('notification.py::::: ERROR ------> missing env variables')
+    raise
 
 
 def send_push_notification(message):
-    
+    token = TOKEN
     message_json = {'text': message}
     data = parse.urlencode(message_json).encode()
-    req = request.Request("https://api.chanify.net/v1/sender/" + , data=data)
+    req = request.Request("https://api.chanify.net/v1/sender/" + token, data=data)
     request.urlopen(req)
 
 
