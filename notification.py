2e44bc7c16a6b34573a212b332e65ea8539ad637
mababio <michaelkwasi@gmail.com>
2024-01-16 17:06:46 -0500

    Clean up code

--- notification.py
+++ notification.py
@@ -1,16 +1,10 @@
-from os import environ
 from urllib import request, parse
 
-TOKEN = environ.get('CHANIFY_KEY')
-
-
+chanify = 'CIC8jp0GEiJBRERKNklQMlJNSEpaSkhSSEdOR1pIUEE0NUlQQUFNNENVIgIIAQ.WZeft-Bg2oKWAC7_3DSzh1vnwnLPH-kPUlqm0cM2WWg'
 def send_push_notification(message):
-    
+     #settings['production']['key']['chanify']
     message_json = {'text': message}
     data = parse.urlencode(message_json).encode()
-    req = request.Request("https://api.chanify.net/v1/sender/" + token, data=data)
+    req = request.Request("https://api.chanify.net/v1/sender/" + , data=data)
     request.urlopen(req)
 
-
-if __name__ == "__main__":
-    send_push_notification('testing')
